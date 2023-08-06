8bdf43ec7a63052d6e84a204bbbcc57b26c25d6d
juiceman1014 <justintran1014@gmail.com>
2023-08-06 14:24:29 -0700

    added a container div that will hold square divs within it. Created a function that will fill the container to make a 16x16 box.

---
+++ javascript.js
@@ -0,0 +1,18 @@
+//Create a webpage with a 16x16 grid of square divs
+//Create a container using html  
+    //fill this container with square divs using javascript
+
+//Step 1: Make a container and fill it with 16 square divs
+const container = document.getElementById("container");
+const numberOfDivs = 256;
+
+function createSquares(containerElement, numberOfDivs){
+    for(let i = 0; i < numberOfDivs; i++){
+        const squareDiv = document.createElement("div");
+        squareDiv.classList.add("square");
+        containerElement.appendChild(squareDiv)
+    }
+}
+
+createSquares(container, numberOfDivs);
+
